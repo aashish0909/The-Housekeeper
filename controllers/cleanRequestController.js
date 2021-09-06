cf22dd49a03a2c400f66548fe8ddafd4426b2e82
aashish0909 <illa.aashish999@gmail.com>
2021-09-07 00:47:36 +0530

    UPDATE - Existence of request using student id

--- controllers/cleanRequestController.js
+++ controllers/cleanRequestController.js
@@ -1,22 +1,26 @@
 const Cleanrequest = require("../models/cleanrequest")
 const student = require("../models/student")
 const validateCreateRequest = require("../validation/cleanrequest")
+const mongoose = require("mongoose")
 
 module.exports.create = async (req, res) => {
-    const { errors, isValid } = await validateCreateRequest(req.body)
+	const { errors, isValid } = await validateCreateRequest(req.body)
 
     if (!isValid) return res.status(400).json(errors)
+    
+    const currStudent = mongoose.Types.ObjectId(req.user.id)
 
-    await Cleanrequest.findOne({ date: req.body.date }).then((request) => {
-        if (request) {
-            return res.status(400).json({ rollNo: "A clean request on this day and at the given time already exists!" })
-        }
-    })
-        
-    let newRequest = await new Cleanrequest({
-        student : req.user.id,
-        date : req.body.date
-    })
-    const savedRequest = await newRequest.save()
-    res.json(savedRequest)
+	await Cleanrequest.findOne({ student: currStudent , date: req.body.date }).then(async (request) => {
+		if (request) {
+			return res
+				.status(400)
+				.json({ error: "A clean request on this day and at the given time already exists!" })
+		}
+		let newRequest = await new Cleanrequest({
+			student: req.user.id,
+			date: req.body.date,
+		})
+		const savedRequest = await newRequest.save()
+		res.json(savedRequest)
+	})
 }
